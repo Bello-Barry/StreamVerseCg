7b8648bf5a534c1112816f803d780be052fddba7
Bello-Barry <bello.barry01@gmail.com>
2025-07-17 22:06:53 +0100

    Ajout de tous les fichiers du projet

--- next.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var nextConfig = {
-    /* config options here */
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-};
-exports.default = nextConfig;
